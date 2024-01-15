377b7b90809e81c9f79a5d4e545d266d17f5b4f2
Luka Momčilović <l.momcilovic61@gmail.com>
2024-01-15 17:55:39 +0100

    feat(config-server): add config-server

---
+++ config-server/src/main/java/com/luka/anirest/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.luka.anirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
